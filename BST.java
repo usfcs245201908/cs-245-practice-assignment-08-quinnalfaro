ac561e25eb558e8d174486d8d893b3054f06466a
Quinn Daniel Alfaro <35777556+quinnalfaro@users.noreply.github.com>
2019-10-22 13:20:14 -0700

    Add files via upload

--- BST.java
+++ BST.java
@@ -1,45 +1,106 @@
-class BST{
-	Node root;
+public class BST<T>{
 
-	public boolean find(int item){
-		return find(item, root);
+	public Node<T> root;
+
+	public BST(){
+		this.root = null;
 	}
 
-	public boolean find(int item, Node node){
-		if(node == null){
-			return false;
-		}
-		if (node.data == item){
-			return true;
-		} else if(node.data < item){
-			return find(item, node.right);
+	public class Node<T> {
+		public Comparable data;
+		public Node<T> left;
+		public Node<T> right;
+
+		public Node(Comparable item){
+			this.data = item;
+			this.left = null;
+			this.right = null;
 		}
 	}
 
-	public void insert(int item){
+	public void insert(Comparable item){
 		root = insert(root, item);
 	}
 
 
-	public Node insert(Node node,int item){
+	public Node insert(Node node, Comparable item){
 		//Base Case
 		if (node == null){
 			return new Node(item);
-		} else if(node.data < item){
-			node.right = insert(node.right, item);
-		} else{
+		} else if(node.data.compareTo(item) > 0){
 			node.left = insert(node.left, item);
+		} else{
+			node.right = insert(node.right, item);
 		}
 		return node;
 	}
 
 	public void print(){
-		//left this right
-		//in order traversal
-
+		print(root);
 	}
 
-	public void delete(comparable thing){
+	public void print(Node node){
+		if(node != null){
+			print(node.left);
+			System.out.println(node.data);
+			print(node.right);
+		}
+	}
 
+
+	public boolean find(Comparable item){
+		return find(item, root);
+	}
+
+	public boolean find(Comparable item, Node node){
+		if(node == null){
+			return false;
+		}
+		if (node.data == item){
+			return true;
+		} else if(node.data.compareTo(item) > 0){
+			return find(item, node.left);
+		} else {
+			return find(item, node.right);
+		} 
+	}
+
+
+	public void delete(Comparable item){
+		root = delete(root, item);
+	}
+
+	public Node delete(Node node, Comparable item){
+		if(node == null){
+			return null;
+		}
+		if(node.data == item){
+			if(node.left == null){
+				return node.right;
+			} else if(node.right == null){
+				return node.left;
+			} else if((node.right).left == null){
+				node.data = (node.right).data;
+				node.right = (node.right).right;
+				return node; 
+			} else{
+				node.data = removeSmallest(node.right);
+				return node;
+			}
+		} else if (node.data.compareTo(item) > 0){
+			node.left = delete(node.left, item);
+		} else {
+			node.right = delete(node.right, item);
+		}
+		return node;
+	}
+
+	public Comparable removeSmallest(Node node){
+		if(node.left.left == null){
+			Comparable smallest = node.left.data;
+			node.left = node.left.right;
+			return smallest;
+		}
+		return removeSmallest(node.left);
 	}
 }
